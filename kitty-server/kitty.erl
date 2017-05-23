7191dec7280d35f5be50b031c331c67ac2360f04
Ashish Negi <ashishnegi33@gmail.com>
2017-05-23 10:41:41 +0530

    Using gen_server for high reliability in serving Cats..

--- kitty-server/kitty.erl
+++ kitty-server/kitty.erl
@@ -1,83 +1,65 @@
 -module(kitty).
--export([start_link/0, order_cat/4, return_cat/2, close_shop/1,
-	 test_boss/0, test_boss_servant/0]).
+-behaviour(gen_server).
+-export([test_boss/0, test_boss_servant/0,
+	 init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 code_change/3, terminate/2]).
 
 -record(cat, {name, color=green, description}).
-
-%%% Generic API
-call(Pid, Msg) ->
-    Ref = erlang:monitor(process, Pid),
-    Pid ! {sync, self(), Ref, Msg},
-    receive
-	{'DOWN', Ref, process, Pid, Reason} ->
-	    erlang:error(Reason);
-	{Ref, Reply} ->
-	    erlang:demonitor(Ref, [flush]),
-	    Reply
-    after 5000 ->
-	    erlang:error(timeout)
-    end.
-
-
-%%% Client API
-start_link() -> spawn_link(fun init/0).
-
-%% Synchronous call
-order_cat(Pid, Name, Color, Description) ->
-    call(Pid, {order, Name, Color, Description}).
-
-%% This call is asynchronous
-return_cat(Pid, Cat = #cat{}) ->
-    Pid ! {async, return, Cat},
-    ok.
-
-%% Synchronous call
-close_shop(Pid) ->
-    call(Pid, terminate).
-
-%%% Server functions
-init() -> loop([]).
-
-handle_cast({return, Cat = #cat{}}, Cats) ->
-    [Cat|Cats].
-
-handle_call({order, Name, Color, Description}, Pid, Ref, Cats) ->
-    if Cats =:= [] ->
-	    Pid ! {Ref, make_cat(Name, Color, Description)},
-	    Cats;
-       Cats =/= [] -> % got to empty the stock
-	    Pid ! {Ref, hd(Cats)},
-	    tl(Cats)
-    end;
-handle_call(terminate, Pid, Ref, Cats) ->
-    Pid ! {Ref, ok},
-    terminate(Cats).
-
-loop(Cats) ->
-    receive
-	{async, Message} ->
-	    loop(handle_cast(Message, Cats));
-	{sync, Pid, Ref, Message} ->
-	    loop(handle_call(Message, Pid, Ref, Cats))
-    end.
 
 %%% Private functions
 make_cat(Name, Col, Desc) ->
     #cat{name=Name, color=Col, description=Desc}.
 
-terminate(Cats) ->
+%%% Client API
+start_link() -> gen_server:start_link(?MODULE, [], []).
+
+%% Synchronous call
+order_cat(Pid, Name, Color, Description) ->
+    gen_server:call(Pid, {order, Name, Color, Description}).
+
+%% This call is asynchronous
+return_cat(Pid, Cat = #cat{}) ->
+    gen_server:cast(Pid, {return_cat, Cat}).
+
+%% Synchronous call
+close_shop(Pid) ->
+    gen_server:call(Pid, terminate).
+
+%%% Server functions
+init([]) -> {ok, []}.
+
+handle_cast({return_cat, Cat = #cat{}}, Cats) ->
+    {noreply, [Cat|Cats]}.
+
+handle_call({order, Name, Color, Description}, _From, Cats) ->
+    if Cats =:= [] ->
+	    {reply, make_cat(Name, Color, Description), Cats};
+       Cats =/= [] -> % got to empty the stock
+	    {reply, hd(Cats), tl(Cats)}
+    end;
+handle_call(terminate, _From, Cats) ->
+    {stop, normal, ok, Cats}.
+
+handle_info(Msg, Cats) ->
+    io:format("Unexpected msg ~p~n", [Msg]),
+    {noreply, Cats}.
+
+terminate(normal, Cats) ->
     [io:format("~p was set free.~n",[C#cat.name]) || C <- Cats],
-    exit(normal).
+    ok.
+
+code_change(_Old, State, _Extra)->
+    {ok, State}.
 
 test_boss()->
-    Server = kitty:start_link(),
+    {ok, Server} = start_link(),
     Boss = order_cat(Server, "Boss cat", "Red", "Bossing around"),
     order_cat(Server, "Servant cat", "White", "Yes boss.."),
     return_cat(Server, Boss),
     close_shop(Server).
 
 test_boss_servant()->
-    Server = kitty:start_link(),
+    {ok, Server} = start_link(),
     Boss = order_cat(Server, "Boss cat", "Red", "Bossing around"),
     Servant = order_cat(Server, "Servant cat", "White", "Yes boss.."),
     return_cat(Server, Servant),
